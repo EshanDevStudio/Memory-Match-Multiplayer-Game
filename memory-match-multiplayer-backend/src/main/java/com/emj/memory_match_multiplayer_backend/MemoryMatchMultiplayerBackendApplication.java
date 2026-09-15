7e5c40aecf2aa4486a444be176e2dd6ac9c0df1b
EshanDevStudio <eshan.devstudio@gmail.com>
2026-09-15 23:54:36 +0530

    backend initial structure

---
+++ memory-match-multiplayer-backend/src/main/java/com/emj/memory_match_multiplayer_backend/MemoryMatchMultiplayerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.emj.memory_match_multiplayer_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoryMatchMultiplayerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoryMatchMultiplayerBackendApplication.class, args);
+	}
+
+}
---
+++ memory-match-multiplayer-backend/src/test/java/com/emj/memory_match_multiplayer_backend/MemoryMatchMultiplayerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emj.memory_match_multiplayer_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoryMatchMultiplayerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
